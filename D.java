a614977a27f7f1f4e06775a8a99ad9a148896c50
Gabriel Silva <107601526+Kyzuke@users.noreply.github.com>
2024-04-06 15:58:11 -0300

    Add class D with methods MD1() and MD2()

---
+++ D.java
@@ -0,0 +1,12 @@
+public class D {
+    private int D1;
+    private float D2;
+
+    public void MD1() {
+        System.out.println("MD1");
+    }
+
+    public void MD2() {
+        System.out.println("MD2");
+    }
+}
