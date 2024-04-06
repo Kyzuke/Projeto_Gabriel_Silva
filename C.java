714a2030c74b6592750f3249c9bf31c11590d204
Gabriel Silva <107601526+Kyzuke@users.noreply.github.com>
2024-04-06 15:40:43 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,12 @@
+public class C {
+    private String C1;
+    private int C2;
+
+    public void MC1() {
+        System.out.println("MC1");
+    }
+
+    public void MC2() {
+        System.out.println("MC2");
+    }
+}
